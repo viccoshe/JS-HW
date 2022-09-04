4ed85240b087d919baa6cd6c4bc2980725af1421
Victoria <viccoshe@gmail.com>
2022-09-04 18:11:01 +0300

    tabs

--- scripts.js
+++ scripts.js
@@ -1,68 +1,33 @@
-window.addEventListener('DOMContentLoaded', function () {
-    const toDoList = function () {
+const tabs = function(selector) {
+    const tabContainer = document.querySelectorAll(selector);
 
-        const container = document.querySelector('.todo-container');
-        const form = document.querySelector('form');
-        const div = document.querySelector('.todo');
-        const save = document.querySelector('.save');
-        const clear = document.querySelector('.clear');
-        const ul = document.createElement('ul');
-        ul.classList.add('to-do-list');
-         
-        form.addEventListener('submit', function(e) {
-            e.preventDefault()
-            const input = document.querySelector('input');
+    const tabHandler = function(buttons, contents) {
+        buttons.forEach(function(btn) {
+            btn.addEventListener('click', function(){
+                buttons.forEach(function(elem){
+                    elem.classList.remove('active');
+                    btn.classList.add('active');
+                });
 
-            if (!input) return;
-            if(!input.value) return;
+                const indexTab = btn.dataset.tab;
 
-            const li = addToDo(input.value);
-
-            ul.append(li);
-            div.append(ul);
-            input.value = '';
-        });
-
-        //adding tasks by user
-        addToDo = function (text) {
-            let li = document.createElement('li');
-            li.classList.add('li');
-            let span = document.createElement('span');
-            span.classList.add('text');
-            span.innerText = text;
-            let btn = document.createElement('button');
-            btn.classList.add('remove-item');
-            btn.innerText = 'Remove';
-            let chkbox = document.createElement('input');
-            chkbox.setAttribute('type', 'checkbox');
-
-            li.append(chkbox, span, btn);
-            return li;
-        }
-
-        //saving tasks
-        saveToDo = function () {
-            localStorage.setItem('todo', ul.innerHTML);
-        }
-        save.addEventListener('click', saveToDo);
-
-        if(localStorage.getItem('todo')) {
-            ul.innerHTML = localStorage.getItem('todo');
-            div.append(ul);
-        }
-        clear.addEventListener('click', function() { //clearing all the tasks
-            localStorage.removeItem('todo');
-            ul.innerHTML = '';
-        });
-
-        ul.addEventListener('click', function(event) { 
-            if (event.target.tagName === 'INPUT') 
-                event.target.nextElementSibling.classList.toggle('complete'); 
-            
-            if (event.target.classList.contains('remove-item'))
-                event.target.parentElement.remove();
+                contents.forEach(function (elem){
+                    if (elem.dataset.tab === indexTab) {
+                        elem.classList.add('active')
+                    }else {
+                        elem.classList.remove('active');
+                    }
+                })
+            });
         });
     }
 
-    toDoList();
-});
+    tabContainer.forEach(function(tab) {
+        const tabButtons = tab.querySelectorAll('.tab');
+        const contents = tab.querySelectorAll('.content');
+
+        tabHandler(tabButtons, contents);
+    });
+}
+
+tabs('.tabs-container');
